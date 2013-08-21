90aca6699369e168db8fcb7c30795b3440717f6a
vgil <vovagil@mail.ru>
2013-08-21 21:28:14 +0300

    add databases

---
+++ Sitecore.SharedSource.VersionManager/sitecore modules/Shell/Sitecore.SharedSource.VersionManager/VersionManager.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Sitecore.SharedSource.VersionManager.sitecore_modules.Shell.Sitecore.SharedSource.VersionManager
+{
+    public partial class VersionManager : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}

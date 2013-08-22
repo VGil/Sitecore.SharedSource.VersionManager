b44d3fb5ce1501a47dc895fad60078e874574aa3
Vladimir Hil <vladimir.hil@lightmaker.com>
2013-08-22 13:10:46 +0300

    implement pipeline

---
+++ Sitecore.SharedSource.VersionManager/sitecore modules/Shell/Sitecore.SharedSource.VersionManager/Logging/InitializeSignalR.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Routing;
+using Sitecore.Pipelines;
+
+namespace Sitecore.SharedSource.VersionManager
+{
+	public class InitializeSignalR
+	{
+		public void Process(PipelineArgs args)
+		{
+			RouteTable.Routes.MapHubs();
+		}
+	}
+}
